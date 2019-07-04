ec111e7b5050b78091b40615b7761d59fe7f3de7
michele-brambilla <michele.brambilla@psi.ch>
2019-07-04 18:49:23 +0200

    Build and run nxfiller in docker

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-/neventGenerator/zmqGenerator /neventGenerator/rita22012n006190.hdf $1 >& /tmp/generator &
-
 cd /sinqhm/
-./rt/sinqhm_filler zmq_socket=tcp://localhost:$1 >& /tmp/sinqhm_filler &
+./rt/sinqhm_filler >& /tmp/sinqhm_filler &
+./nxfiller rita22012n006190.hdf /entry1/RITA-2/detector/counts >& /tmp/filler &
 cd ua
 ./sinqhmegi >& /tmp/sinqhm_server
